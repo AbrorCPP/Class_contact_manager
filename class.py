cf1e8f7806cb33d0b95587de19d616cc2247ff5e
AbrorCPP <abrorboltayev4843@gmail.com>
2025-11-19 16:54:48 +0500

    123

--- class.py
+++ class.py
@@ -1,113 +1,136 @@
 class Contact:
-    def __init__(self, name, phone,id):
+    def __init__(self, name, phone, id):
         self.name = name
         self.phone = phone
         self.id = id
+
     def __str__(self):
-        return "to'g'ri print qil"
+        return f"{self.id}. {self.name}: {self.phone}"
+
 
 class SMS:
-    def __init__(self, name, phone, massage,id):
+    def __init__(self, name, phone, message, id):
         self.name = name
         self.phone = phone
-        self.massage = massage
+        self.message = message
         self.id = id
+
     def __str__(self):
-        return "xato"
+        return f"{self.id}. {self.name} ({self.phone}): {self.message}"
+
 
 class Main:
     def __init__(self):
         self.contacts = []
-        self.massages = []
+        self.messages = []
 
+    # IDlarni qayta tartiblash (0,1,2,...)
+    def fix_ids_contacts(self):
+        for i, c in enumerate(self.contacts):
+            c.id = i
+
+    def fix_ids_sms(self):
+        for i, m in enumerate(self.messages):
+            m.id = i
+
+    # Contact qo'shish
     def con_add(self):
-        n = input("Enter the username: ")
-        n1 = input("Enter the phone: ")
-        s = Contact(n, n1,0)
-        self.contacts.append(s)
+        name = input("Enter username: ")
+        phone = input("Enter phone: ")
+        self.contacts.append(Contact(name, phone, len(self.contacts)))
+        print("Added!")
 
-    def idd_corr(self):
-        t = []
-        for i in range(len(self.contacts)):
-            b = i+1
-            s = Contact(self.contacts[b-1].name, self.contacts[b-1].phone,i)
-            t.append(s)
-        self.contacts.clear()
-        self.contacts.append(t)
-
+    # Contactlarni chiqarish
     def con_print(self):
+        if not self.contacts:
+            print("No contacts.")
         for c in self.contacts:
-            print(f"{c.name}: {c.phone}")
+            print(c)
 
+    # Contact tahrirlash
     def con_edit(self):
-        a = input("Enter the contact: ")
+        phone = input("Enter contact phone: ")
         for c in self.contacts:
-            if c.phone == a:
-                s = input("Enter the new username: ")
-                s1 = input("Enter the new phone: ")
-                a2 = Contact(s, s1)
-                self.contacts.remove(c)
-                self.contacts.append(a2)
+            if c.phone == phone:
+                new_name = input("Enter new username: ")
+                new_phone = input("Enter new phone: ")
+                c.name = new_name
+                c.phone = new_phone
+                print("Updated!")
+                return
+        print("Not found!")
 
+    # Contact o'chirish
     def del_contact(self):
-        a = input("Enter the contact: ")
+        phone = input("Enter contact phone: ")
         for c in self.contacts:
-            if c.phone == a:
+            if c.phone == phone:
                 self.contacts.remove(c)
+                self.fix_ids_contacts()
+                print("Deleted!")
+                return
+        print("Not found!")
 
-    def idd_corr_sms(self):
-        t = []
-        for i in range(len(self.massages)):
-            b = i+1
-            s = SMS(self.massages[b-1].name, self.massages[b-1].phone,self.massages[b-1].massage,i)
-            t.append(s)
-        self.massages.clear()
-        self.massages.append(t)
-
+    # SMS jo'natish
     def send_sms(self):
-        t = input("Enter the number: ")
+        phone = input("Enter number: ")
         for c in self.contacts:
-            if c.phone == t:
-                t1 = input("Enter the text: ")
-                t2 = SMS(c.name, t, t1,0)
-                self.massages.append(t2)
+            if c.phone == phone:
+                msg = input("Enter message: ")
+                self.messages.append(SMS(c.name, phone, msg, len(self.messages)))
+                print("SMS sent!")
+                return
+        print("Contact not found!")
 
+    # SMSlarni chiqarish
     def print_sms(self):
-        for m in self.massages:
-            print(f"ID.{m.id}  {m.name}: {m.phone}, {m.massage}")
+        if not self.messages:
+            print("No SMS.")
+        for m in self.messages:
+            print(m)
 
-    def del_massages(self):
-        t = int(input("Enter the id: "))
-        for m in self.massages:
-            if m.id == t:
-                self.massages.remove(m)
+    # SMS o'chirish
+    def del_sms(self):
+        sms_id = int(input("Enter SMS ID: "))
+        for m in self.messages:
+            if m.id == sms_id:
+                self.messages.remove(m)
+                self.fix_ids_sms()
+                print("SMS deleted!")
+                return
+        print("Not found!")
+
 
 m1 = Main()
 
 while True:
-    a = input(" 1.Contacts\n 2.SMS\n3.Exit\n--->")
+    a = input("\n1. Contacts\n2. SMS\n3. Exit\n---> ")
+
     if a == "1":
         while True:
-            a1 = input("1.Add contact\n2.Print contact\n3.Edit contact\n4.Delete contact\n5.Exit\n--->")
-            if a == "1":
+            a1 = input("\n1. Add contact\n2. Print contacts\n3. Edit\n4. Delete\n5. Exit\n---> ")
+            if a1 == "1":
                 m1.con_add()
-            elif a == "2":
+            elif a1 == "2":
                 m1.con_print()
-            elif a == "3":
+            elif a1 == "3":
                 m1.con_edit()
-            elif a == "4":
+            elif a1 == "4":
                 m1.del_contact()
             else:
                 break
-    if a == "2":
+
+    elif a == "2":
         while True:
-            m1.idd_corr_sms()
-            a2 = input("1.Send SMS\n2.Delete SMS\n3.Exit\n--->")
-            if a == "1":
+            a2 = input("\n1. Send SMS\n2. Print SMS\n3. Delete SMS\n4. Exit\n---> ")
+            if a2 == "1":
                 m1.send_sms()
-            elif a == "2":
-                m1.del_massages()
+            elif a2 == "2":
+                m1.print_sms()
+            elif a2 == "3":
+                m1.del_sms()
             else:
                 break
+
     else:
         break
